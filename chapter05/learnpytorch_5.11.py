211ae7e9d45f02d3848c24dbee65efd91aa0da18
suchang <suchang@autocore.ai>
2020-01-17 14:41:58 +0800

    1.nn.AdaptiveAvgPool2d(1)代替AvgPool2d以适应不同size的输入

--- chapter05/learnpytorch_5.11.py
+++ chapter05/learnpytorch_5.11.py
@@ -77,7 +77,8 @@ class ResNet(nn.Module):
             Residual(512,512),
         )
 
-        self.avg_pool = nn.AvgPool2d(kernel_size=7)
+        # self.avg_pool = nn.AvgPool2d(kernel_size=7)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1) #代替AvgPool2d以适应不同size的输入
         self.fc = nn.Linear(512,num_classes)
 
     def forward(self,x):
@@ -94,86 +95,74 @@ class ResNet(nn.Module):
 
         return out
 
-net = ResNet(1,10)
-X=torch.randn((1,1,224,224))
-out = net(X)
-print(out.shape)
+# net = ResNet(1,10)
+# X=torch.randn((1,1,128,128))
+# out = net(X)
+# print(out.shape)
 
+## 数据加载
+batch_size,num_workers=32,2
+train_iter,test_iter = learntorch_utils.load_data(batch_size,num_workers,resize=128)
+print('load data done,batch_size:%d' % batch_size)
 
+## 模型定义
+net = ResNet(1,10).cuda()
 
-# ## 数据加载
-# batch_size,num_workers=4,2
-# train_iter,test_iter = learntorch_utils.load_data(batch_size,num_workers,None)
+## 损失函数定义
+l = nn.CrossEntropyLoss()
 
-# ## 模型定义
-# class LeNet(nn.Module):
-#     def __init__(self):
-#         super(LeNet, self).__init__()
-#         self.conv = nn.Sequential(
-#             nn.Conv2d(1, 6, 5), # in_channels, out_channels, kernel_size
-#             nn.BatchNorm2d(6),
-#             nn.Sigmoid(),
-#             nn.MaxPool2d(2, 2), # kernel_size, stride
-#             nn.Conv2d(6, 16, 5),
-#             nn.BatchNorm2d(16),
-#             nn.Sigmoid(),
-#             nn.MaxPool2d(2, 2)
-#         )
-#         self.fc = nn.Sequential(
-#             nn.Linear(16*4*4, 120),
-#             nn.BatchNorm1d(120),
-#             nn.Sigmoid(),
-#             nn.Linear(120, 84),
-#             nn.BatchNorm1d(84),
-#             nn.Sigmoid(),
-#             nn.Linear(84, 10)
-#         )
+## 优化器定义
+opt = torch.optim.Adam(net.parameters(),lr=0.01)
 
-#     def forward(self, img):
-#         feature = self.conv(img)
-#         output = self.fc(feature.view(img.shape[0], -1))
-#         return output
+## 评估函数定义
+num_epochs=5
+def test():
+    acc_sum = 0
+    batch = 0
+    for X,y in test_iter:
+        X,y = X.cuda(),y.cuda()
+        y_hat = net(X)
+        acc_sum += (y_hat.argmax(dim=1) == y).float().sum().item()
+        batch += 1
+    
+    test_acc = acc_sum/(batch*batch_size)
 
-# net = LeNet().cuda()
+    # print('test acc:%f' % test_acc)
+    return test_acc
 
-# ## 损失函数定义
-# l = nn.CrossEntropyLoss()
+## 训练
+def train():
+    for epoch in range(num_epochs):
+        train_l_sum,batch,train_acc_sum=0,1,0
+        start = time.time()
+        for X,y in train_iter:
+            X,y = X.cuda(),y.cuda() #把tensor放到显存
+            y_hat = net(X)  #前向传播
+            loss = l(y_hat,y) #计算loss,nn.CrossEntropyLoss中会有softmax的操作
+            opt.zero_grad()#梯度清空
+            loss.backward()#反向传播,求出梯度
+            opt.step()#根据梯度,更新参数
 
-# ## 优化器定义
-# opt = torch.optim.Adam(net.parameters(),lr=0.01)
+            #　数据统计
+            train_l_sum += loss.item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).float().sum().item()
+            train_loss = train_l_sum/(batch*batch_size)
+            train_acc = train_acc_sum/(batch*batch_size)
+            
+            if batch % 100 == 0: #每100个batch输出一次训练数据
+                print('epoch %d,batch %d,train_loss %.3f,train_acc:%.3f' % (epoch,batch,train_loss,train_acc))
 
-# ## 评估函数定义
-# num_epochs=5
-# def test():
-#     acc_sum = 0
-#     batch = 0
-#     for X,y in test_iter:
-#         X,y = X.cuda(),y.cuda()
-#         y_hat = net(X)
-#         acc_sum += (y_hat.argmax(dim=1) == y).float().sum().item()
-#         batch += 1
-#     print('test acc:%f' % (acc_sum/(batch*batch_size)))
+            if batch % 300 == 0: #每300个batch测试一次
+                test_acc = test()
+                print('epoch %d,batch %d,test_acc:%.3f' % (epoch,batch,test_acc))
 
-# ## 训练
-# def train():
-#     for epoch in range(num_epochs):
-#         train_l_sum,batch=0,0
-#         start = time.time()
-#         for X,y in train_iter:
-#             X,y = X.cuda(),y.cuda() #把tensor放到显存
-#             y_hat = net(X)  #前向传播
-#             loss = l(y_hat,y) #计算loss,nn.CrossEntropyLoss中会有softmax的操作
-#             opt.zero_grad()#梯度清空
-#             loss.backward()#反向传播,求出梯度
-#             opt.step()#根据梯度,更新参数
+            batch += 1
 
-#             train_l_sum += loss.item()
-#             batch += 1
-#         end = time.time()
-#         time_per_epoch =  end - start
-#         print('epoch %d,batch_size %d,train_loss %f,time %f' % 
-#                 (epoch + 1,batch_size ,train_l_sum/(batch*batch_size),time_per_epoch))
-#         test()
+        end = time.time()
+        time_per_epoch =  end - start
+        print('epoch %d,batch_size %d,train_loss %f,time %f' % 
+                (epoch + 1,batch_size ,train_l_sum/(batch*batch_size),time_per_epoch))
+        test()
 
-# train()
+train()
 
