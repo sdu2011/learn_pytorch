00dc148dcf4c4892f2bdd4058dd79faf4f7488c7
suchang <suchang@autocore.ai>
2020-01-17 13:37:44 +0800

    resnet conv3,Residual增加1x1卷积调整输入shape

--- chapter05/learnpytorch_5.11.py
+++ chapter05/learnpytorch_5.11.py
@@ -9,23 +9,33 @@ import torchvision.models as models
 models.resnet
 
 class Residual(nn.Module):
-    def __init__(self,in_channels,out_channels):
+    def __init__(self,in_channels,out_channels,stride=1):
         super(Residual,self).__init__()
-        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=1,padding=1)
+        self.stride = stride
+        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=stride,padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3,padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
+        #　ｘ卷积后shape发生改变,比如:x:[1,64,56,56] --> [1,128,28,28],则需要1x1卷积改变x
+        if in_channels != out_channels:
+            self.conv1x1 = nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=stride)
+        else:
+            self.conv1x1 = None
+            
+
     def forward(self,x):
-        print(x.shape)
+        # print(x.shape)
         o1 = self.relu(self.bn1(self.conv1(x)))
-        print(o1.shape)
+        # print(o1.shape)
         o2 = self.bn2(self.conv2(o1))
-        print(o2.shape)
+        # print(o2.shape)
+
+        if self.conv1x1:
+            x = self.conv1x1(x) 
 
         out = self.relu(o2 + x)
-        
         return out
 
 class ResNet(nn.Module):
@@ -44,9 +54,19 @@ class ResNet(nn.Module):
             Residual(64,64),
         )
 
+        self.conv3 = nn.Sequential(
+            Residual(64,128,stride=2),
+            Residual(128,128),
+            Residual(128,128),
+            Residual(128,128),
+            Residual(128,128),
+        )
+
     def forward(self,x):
         out = self.conv1(x)
         out = self.conv2(out)
+        out = self.conv3(out)
+
         return out
 
 net = ResNet(1)
